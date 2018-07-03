a1a78cc11d231f7c043d4ec2e127c30227cfb764
james ngacha <jazooh@gmail.com>
2018-07-03 13:08:42 +0300

    init7

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
-app.get('/', (req,res)=>{
-  res.send('it works!');
+app.get('/', (req, res) => {
+  res.send('It Works!');
 });
 
-const port =process.env.PORT || 5000;
+// Use Routes
+app.use('/auth', auth);
 
-app.listen(port, ()=>{
-console.log(`Server started on port ${port}`)
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
 });
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.export ={
+  mongoURI:'mongodb://james:james500@ds125241.mlab.com:25241/storybooks-dev',
+  googleClientID:'45333001275-cadtl97haurq1lts2mce6q9a02d3g4qb.apps.googleusercontent.com',
+  googleClientSecret:'qUn_XlI7gv2W4hvioolZ23TY'
+}
