a2880141681271ec279a89fced8dc14473b7fae3
james ngacha <jazooh@gmail.com>
2018-07-05 12:27:55 +0300

    final

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports ={
-  mongoURI:'mongodb://james:james500@ds125241.mlab.com:25241/storybooks-dev',
+  mongoURI:'mongodb://kimathi:kimathi500@ds125241.mlab.com:25241/storybooks-dev',
   googleClientID:'45333001275-cadtl97haurq1lts2mce6q9a02d3g4qb.apps.googleusercontent.com',
   googleClientSecret:'qUn_XlI7gv2W4hvioolZ23TY'
 }
